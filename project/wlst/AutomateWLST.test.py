e06f745d8c840e1b0ee59489195805a1b114dd1d
Istvan Percsi <istvanpercsi@gmail.com>
2018-11-13 21:38:14 +0100

    read Properties file is working.

--- project/wlst/AutomateWLST.test.py
+++ project/wlst/AutomateWLST.test.py
@@ -3,73 +3,10 @@ import sys
 from ip.common.common import Common
 from ip.common.extendedobject import ExtendedObject
 from ip.logger.logger import Logger
+from ip.common.properties import Properties
 
 
 Logger.setLogLevel('trace')
 
-d = dict()
-d['x.y'] = 'z'
-x = ExtendedObject(d)
-print(x.x.y)
-x.k = 'f'
-print(x.k)
-x.x = 'z'
-print(x.x)
-
-d = {'s':{'t':'V'}}
-x = ExtendedObject(d)
-print(x.s.t)
-x.s.update(ExtendedObject({'z':{'k':'o','t':'tt'}}))
-print(x.s.z.k)
-
-print(x.s.z.t) 
-print(x.s.t)
-
-x.s.update(ExtendedObject({'z':{'k':'i'}}))
-print(x.s.z.k)
-
-print(x.s.z.t) 
-print(x.s.t)
-
-
-
-
-
-
-
-
-
-
-
-
-
-#Conditionally import wlstModule only when script is executed with jython
-'''
-if __name__ == '__main__': 
-    from wlstModule import *#@UnusedWildImport
-
-print 'starting the script ....'
-username = 'weblogic'
-password = 'welcome1'
-url='t3://localhost:7001'
-
-connect(username,password,url)
-
-
-edit()
-startEdit()
-
-
-
-
-try:
-    save()
-    activate(block="true")
-    print "script returns SUCCESS"   
-except Exception, e:
-    print e 
-    print "Error while trying to save and/or activate!!!"
-    dumpStack()
-    raise 
-    
-'''
+p = Properties()
+p.readPropertiesFromFile(sys.argv[1])
--- project/wlst/ip/common/properties.py
+++ project/wlst/ip/common/properties.py
@@ -5,21 +5,26 @@ Created on Oct 28, 2018
 '''
 import re
 from ip.common.extendedobject import ExtendedObject
+from ip.logger.logger import Logger
 
 class Properties(ExtendedObject):
     '''
     Properties Object. This is an extension for Extended Object.
-    This object can process key value list from properties file, and comma or semicolon separated key value list.
+    This object can process key value list from properties file, or comma or semicolon separated list.
     '''
 
+    logger = Logger.getInstance('Properties')
+
     def readPropertiesFromFile(self,fileName):
+        self.logger.trace('Filename: ' + str(fileName))
         propertiesFile = open(fileName)
         propertiesList = list()
         for propertyLine in propertiesFile:
+            self.logger.trace('Property line: ' + str(propertyLine))
             propertyLine = propertyLine.strip()
-            regex = re.compile('^\s(!|#).*$')
-            if not len(propertyLine) != 0 and not regex.match(propertyLine):   
-                self.__propertiesList.append(propertyLine)
+            regex = re.compile('^\s*[!#]+.*$')
+            if len(propertyLine) != 0 and not regex.match(propertyLine):   
+                propertiesList.append(propertyLine)
         self.__processPropertiesList(propertiesList)
     
     def readPropertiesFromSeparatedKeyValueList(self,separatedKeyValueList):
@@ -28,4 +33,6 @@ class Properties(ExtendedObject):
         
     def __processPropertiesList(self,propertiesList):
         print('---> TODO')
+        self.logger.trace(str(propertiesList))
+        
         
