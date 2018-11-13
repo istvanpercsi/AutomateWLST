e1eb0137e9d2252e575fef9c12809f1de65e0bf2
Istvan Percsi <istvanpercsi@gmail.com>
2018-11-13 21:54:21 +0100

    read pairs of key-value from file is working

--- project/wlst/AutomateWLST.test.py
+++ project/wlst/AutomateWLST.test.py
@@ -3,10 +3,12 @@ import sys
 from ip.common.common import Common
 from ip.common.extendedobject import ExtendedObject
 from ip.logger.logger import Logger
-from ip.common.properties import Properties
+from ip.common.properties import Attributes
 
 
 Logger.setLogLevel('trace')
 
-p = Properties()
-p.readPropertiesFromFile(sys.argv[1])
+p = Attributes()
+p.readAttributesFromFile(sys.argv[1])
+print(p.parameter2)
+print(p.param1.param11)
--- project/wlst/ip/common/properties.py
+++ project/wlst/ip/common/properties.py
@@ -7,32 +7,37 @@ import re
 from ip.common.extendedobject import ExtendedObject
 from ip.logger.logger import Logger
 
-class Properties(ExtendedObject):
+class Attributes(ExtendedObject):
     '''
-    Properties Object. This is an extension for Extended Object.
+    Attributes Object. This is an extension for Extended Object.
     This object can process key value list from properties file, or comma or semicolon separated list.
     '''
 
-    logger = Logger.getInstance('Properties')
+    logger = Logger.getInstance('Attributes')
 
-    def readPropertiesFromFile(self,fileName):
+    def readAttributesFromFile(self,fileName):
         self.logger.trace('Filename: ' + str(fileName))
-        propertiesFile = open(fileName)
-        propertiesList = list()
-        for propertyLine in propertiesFile:
-            self.logger.trace('Property line: ' + str(propertyLine))
-            propertyLine = propertyLine.strip()
+        attributesFile = open(fileName)
+        attributesList = list()
+        for attributeLine in attributesFile:
+            self.logger.trace('Property line: ' + str(attributeLine))
+            attributeLine = attributeLine.strip()
             regex = re.compile('^\s*[!#]+.*$')
-            if len(propertyLine) != 0 and not regex.match(propertyLine):   
-                propertiesList.append(propertyLine)
-        self.__processPropertiesList(propertiesList)
+            if len(attributeLine) != 0 and not regex.match(attributeLine): 
+                self.logger.trace('Valid property line: ' + str(attributeLine))  
+                attributesList.append(attributeLine)
+        self.__processAttributesList(attributesList)
     
-    def readPropertiesFromSeparatedKeyValueList(self,separatedKeyValueList):
-        propertiesList = re.split(r'[,;]+', separatedKeyValueList)
-        self.__processPropertiesList(propertiesList)
+    def readAttributesFromSeparatedKeyValueList(self,separatedKeyValueList):
+        attributesList = re.split(r'[,;]+', separatedKeyValueList)
+        self.__processAttributesList(attributesList)
         
-    def __processPropertiesList(self,propertiesList):
-        print('---> TODO')
-        self.logger.trace(str(propertiesList))
+    def __processAttributesList(self,attributesList):
+        self.logger.trace(str(attributesList))
+        for attribute in attributesList:
+            keyValuePair = re.split(r'[:=]+', attribute)
+            self.logger.trace('Key-Value: ' + str(keyValuePair))
+            self.__setattr__(str(keyValuePair[0]).strip(), str(keyValuePair[1]).strip(), True)
+        
         
         
