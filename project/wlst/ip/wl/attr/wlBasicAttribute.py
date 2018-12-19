0d3c6f2bf87569b6c735957edf33efdd6179a023
Istvan Percsi <istvanpercsi@gmail.com>
2018-12-19 21:14:43 +0100

    wlBasicAttributes

--- project/wlst/ip/wl/attr/wlBasicAttribute.py
+++ project/wlst/ip/wl/attr/wlBasicAttribute.py
@@ -3,48 +3,73 @@ Created on Dec 17, 2018
 
 @author: peris
 '''
+import wlstModule as wlst
 from ip.common.extendedobject import ExtendedObject
+from ip.logger.logger import Logger
 
 class WLBasicAttribute(ExtendedObject):
     '''
-    classdocs
+    Basic Attribute Class for attributes of resource with basic functions.
     '''
-
-
-    def __init__(self, path, value):
+    def __init__(self, name, value = None):
         '''
         Constructor
         '''
-        self.__path = path
-        self.__value = value
+        self.logger = Logger.getInstance()
+        self.__name = self.setName(name)
+        self.__value = self.setValue(value)
     
     def set(self):
-        raise NotImplementedError('WLBasicAttribute.set() has not been implemented!')
+        '''
+        Set value of attribute in Weblogic. Context must be set.
+        '''
+        wlst.set(self.__name,self.__value)
     
     def setInWL(self):
+        '''
+        Alias for WLBasicAttributes.set()
+        '''
         self.set()
     
     def get(self):
-        raise NotImplementedError('WLBasicAttribute.get() has not been implemented!')
+        '''
+        Get value of attribute from WebLogic. Context must be set.
+        '''
+        self.__value = wlst.get(self.__name)
     
     def getFromWL(self):
+        '''
+        Alias for WLBasicAttributes.get()
+        '''
         self.get()
     
     def compare(self):
-        raise NotImplementedError('WLBasicAttribute.compare() has not been implemented!')
+        '''
+        Compare actual value with value from WebLogic
+        '''
+        if self.value == wlst.get(self.__name):
+            return True
+        else:
+            return False
     
     def compareWithWL(self):
+        '''
+        Alias for WLBasicAttributes.compare()
+        '''
         self.compare()
     
-    def setPath(self, path):
-        raise NotImplementedError('WLBasicAttribute.setPath() has not been implemented!')
+    def setName(self, name):
+        if isinstance(name,str):
+            self.__name = name
+        else:
+            self.logger.error('Value of parameter \'name\' must be string.')
     
-    def getPath(self):
-        raise NotImplementedError('WLBasicAttribute.getPath() has not been implemented!')
+    def getName(self):
+        return self.__name
     
     def setValue(self, value):
-        raise NotImplementedError('WLBasicAttribute.setValue() has not been implemented!')
+        self.__value = value
     
     def getValue(self):
-        raise NotImplementedError('WLBasicAttribute.getValue() has not been implemented!')
+        return self.value
     
