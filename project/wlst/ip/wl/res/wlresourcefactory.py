5c38b55a485806c3d523d199d91d777d51747b14
Istvan Percsi <istvanpercsi@gmail.com>
2019-01-27 21:47:22 +0100

    New class for WL Resources Factory

---
+++ project/wlst/ip/wl/res/wlresourcefactory.py
@@ -0,0 +1,17 @@
+'''
+Created on 27.01.2019
+
+@author: istva
+'''
+from ip.common.extendedobject import ExtendedObject
+
+class WLResourceFactory(ExtendedObject):
+    '''
+    classdocs
+    '''
+
+    def __init__(self,params):
+        '''
+        Constructor
+        '''
+        
