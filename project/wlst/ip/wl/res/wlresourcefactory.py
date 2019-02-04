0ad98ff7b9784f7782d90ed645b8d834a26d092c
Istvan Percsi <istvanpercsi@gmail.com>
2019-02-04 10:54:59 +0100

    WLResourceFactory - createResources
    method has been added.

--- project/wlst/ip/wl/res/wlresourcefactory.py
+++ project/wlst/ip/wl/res/wlresourcefactory.py
@@ -4,14 +4,23 @@ Created on 27.01.2019
 @author: istva
 '''
 from ip.common.extendedobject import ExtendedObject
+from ip.logger.logger import Logger
 
 class WLResourceFactory(ExtendedObject):
     '''
     classdocs
     '''
 
-    def __init__(self,params):
-        '''
-        Constructor
-        '''
-        
+    def createResources(resources):
+        logger = Logger('WLResourceFactory')
+        logger.debug('Resources: ' + str(resources))
+        logger.debug('Resoruces type: ' + str(type(resources)))
+        logger.debug(resources.resources.res01.name)
+        for k,resource in resources.iterEOItems():
+            logger.debug('Key: ' + str(k))
+            logger.debug('Type: ' + str(type(resource)))
+    
+    createResources = staticmethod(createResources)
+            
+            
+            
