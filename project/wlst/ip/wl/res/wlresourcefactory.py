50cc10e711ffb9ee3e03de4f1854aed1bb97786d
Istvan Percsi <istvanpercsi@gmail.com>
2019-02-04 21:33:09 +0100

    WLResourceFactory - createResources
    Skeleton of Class is ready

--- project/wlst/ip/wl/res/wlresourcefactory.py
+++ project/wlst/ip/wl/res/wlresourcefactory.py
@@ -1,26 +1,37 @@
 '''
 Created on 27.01.2019
 
-@author: istva
+@author: Istvan Percsi
 '''
 from ip.common.extendedobject import ExtendedObject
 from ip.logger.logger import Logger
+from ip.wl.res.wlresourcetypes import WLResourceTypes
 
 class WLResourceFactory(ExtendedObject):
     '''
-    classdocs
+    WLResourceFactory - this class will create resources from Attribute object.
     '''
 
-    def createResources(resources):
+    def createResources(resources,typeOfResource = None):
         logger = Logger('WLResourceFactory')
-        logger.debug('Resources: ' + str(resources))
-        logger.debug('Resoruces type: ' + str(type(resources)))
-        logger.debug(resources.resources.res01.name)
-        for k,resource in resources.iterEOItems():
-            logger.debug('Key: ' + str(k))
-            logger.debug('Type: ' + str(type(resource)))
-    
+        for resKey,resValue in resources.iterEOItems():
+            if typeOfResource == None or str(resKey).lower() == str(WLResourceTypes.getTypeOfResources(typeOfResource)).lower():
+                if str(resKey).lower() == str(WLResourceTypes.getTypeOfResources(WLResourceTypes.CLUSTER)).lower():
+                    logger.debug('Create resource: ' + WLResourceTypes.CLUSTER)
+                elif str(resKey).lower() == str(WLResourceTypes.getTypeOfResources(WLResourceTypes.SERVER)).lower():
+                    logger.debug('Create resource: ' + WLResourceTypes.SERVER)
+                elif str(resKey).lower() == str(WLResourceTypes.getTypeOfResources(WLResourceTypes.DATASOURCE)).lower():
+                    logger.debug('Create resource: ' + WLResourceTypes.DATASOURCE)
+                # ONLY for TEST BEGIN
+                elif str(resKey).lower() == str(WLResourceTypes.getTypeOfResources(WLResourceTypes.RESOURCE)).lower():
+                    logger.debug('Create resource: ' + WLResourceTypes.RESOURCE)
+                # ONLY for TEST END    
+                else:
+                    logger.debug('Key: ' + str(resKey))
+                    logger.debug('Type: ' + str(type(resValue)))
+
     createResources = staticmethod(createResources)
+
             
             
             
