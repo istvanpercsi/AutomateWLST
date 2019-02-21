2ed8662a4b5719de616f394c7eee2bb0d81b8083
Istvan Percsi <istvanpercsi@gmail.com>
2019-02-21 22:16:51 +0100

    Test

--- project/wlst/AutomateWLST.test.py
+++ project/wlst/AutomateWLST.test.py
@@ -1,16 +1,9 @@
-import sys
 
-from ip.common.common import Common
-from ip.common.extendedobject import ExtendedObject
 from ip.logger.logger import Logger
 from ip.common.attributes import Attributes
-from ip.wl.wlcommon import WLCommon
-from ip.wl.err.common.resourcecouldnotbestartederror import ResourceCouldNotBeStartedError
-from ip.wl.res.wlbasicresource import WLBasicResource
+from ip.wl.res.wlresourcefactory import WLResourceFactory
 
 Logger.setLogLevel('trace')
-#raise ResourceCouldNotBeStartedError('Resource busy')
-#WLCommon.connect("weblogic","Weblogic1","t3://localhost:7001")
-
-x = WLBasicResource('test','test2')
-x.setAttributesInWL()
+attr = Attributes()
+attr.readAttributesFromFile("config.properties")
+WLResourceFactory.createResources(attr)
